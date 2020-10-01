5adce3f42fc6078a4a0075d122df0d750044b49f
GauthierQ <queau.gauthier@orange.fr>
2020-10-01 16:57:34 +0200

    terrain list

--- lib/menu/listTerrain.dart
+++ lib/menu/listTerrain.dart
@@ -31,50 +31,59 @@ class _ListTerrainPageState extends State<ListTerrainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-          child: FirebaseAnimatedList(
-              shrinkWrap: true,
-              query: _terrainRef,
-              itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                  Animation<double> animation, int index) {
-                return new Card(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              snapshot.value['nom'],
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            Text(
-                              snapshot.value['adresse'],
+      body: Column(
+        children: [
+          Container(
+            color: Colors.amber,
+            height: MediaQuery.of(context).size.height*0.2,
+          ),
+          SingleChildScrollView(
+              child: FirebaseAnimatedList(
+                  shrinkWrap: true,
+                  query: _terrainRef,
+                  itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                      Animation<double> animation, int index) {
+                    return new Card(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  snapshot.value['nom'],
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.value['adresse'],
 
+                                ),
+                                Text(
+                                  snapshot.value['ville'],
+                                ),
+                                snapshot.value['etat'] == 1
+                                    ? Text("Mauvais")
+                                    : Text("Bon"),
+                              ],
                             ),
-                            Text(
-                              snapshot.value['ville'],
-                            ),
-                            snapshot.value['etat'] == 1
-                                ? Text("Mauvais")
-                                : Text("Bon"),
-                          ],
-                        ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () =>
+                                _terrainRef.child(snapshot.key).remove(),
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () =>
-                            _terrainRef.child(snapshot.key).remove(),
-                      ),
-                    ],
-                  ),
-                );
-              })),
+                    );
+                  })),
+        ],
+      )
+
     );
   }
 }
