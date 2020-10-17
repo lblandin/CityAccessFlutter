b4d2b06f84f32ce676df6f6f13704a7d011799dc
GauthierQ <queau.gauthier@orange.fr>
2020-10-17 12:08:44 +0200

    Add marker

--- lib/menu/add.dart
+++ lib/menu/add.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:CityAccess/model/terrain.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -13,17 +16,57 @@ class _AddPageState extends State<AddPage> {
 
   final DatabaseReference database = FirebaseDatabase.instance.reference().child("Terrain");
 
+  List<Terrain> terrains = [];
+
+  @override
+  initState(){
+
+    DatabaseReference reference = FirebaseDatabase.instance.reference();
+    reference.child('Terrain').once().then((DataSnapshot snapshot) {
+      var keys = snapshot.value.keys;
+      var data = snapshot.value;
+      terrains.clear();
+      for (var key in keys) {
+        Terrain terrain = new Terrain(
+            data[key]["id"],
+            data[key]["nom"],
+            data[key]["description"],
+            data[key]["adresse"],
+            data[key]["cp"],
+            data[key]["ville"],
+            data[key]["etat"],
+            data[key]["latitude"],
+            data[key]["longitude"]);
+        terrains.add(terrain);
+
+      }
+      setState(() {
+        print('lenght terrains: ${terrains.length}');
+      });
+    });
+
+    super.initState();
+  }
+
+  double lat = Random.secure().nextDouble() * (49 - 47) + 47;
+  double lng = Random.secure().nextDouble() * (-0.5 - (-1.5)) + 1.5;
+
+  int index;
+
   sendField() {
+    setState(() {
+      index = terrains.length+1;
+    });
     database.push().set({
-      'id' : 1,
-      'nom' : 'Test',
+      'id' : index,
+      'nom' : 'Test'+lat.toString(),
       'description' : 'Blabla',
       'adresse' : '4 allée des fleurs',
       'cp' : 35000,
       'ville' : 'Rennes',
       'etat' : 3,
-      'latitude' : 48.0359541,
-      'longitude' : -1.8419968
+      'latitude' : lat,
+      'longitude' : lng,
     });
   }
 
