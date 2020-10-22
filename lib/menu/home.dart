4475a50f0c13eb53ccb2ad7b5ead051399b6f6b1
GauthierQ <queau.gauthier@orange.fr>
2020-10-22 08:55:22 +0200

    erqzrethry

--- lib/menu/home.dart
+++ lib/menu/home.dart
@@ -1,22 +1,56 @@
 import 'package:CityAccess/maps.dart';
+import 'package:CityAccess/model/terrain.dart';
 import 'package:CityAccess/rules/basketball.dart';
 import 'package:CityAccess/rules/football.dart';
 import 'package:CityAccess/rules/handball.dart';
 import 'package:CityAccess/rules/volleyball.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Terrain> terrains = [];
+
+  @override
+  void initState() {
+    DatabaseReference reference = FirebaseDatabase.instance.reference();
+    reference.child('Terrain').once().then((DataSnapshot snapshot) {
+      var keys = snapshot.value.keys;
+      var data = snapshot.value;
+      terrains.clear();
+      for (var key in keys) {
+        Terrain terrain = new Terrain(
+            data[key]["id"],
+            data[key]["nom"],
+            data[key]["description"],
+            data[key]["adresse"],
+            data[key]["cp"],
+            data[key]["ville"],
+            data[key]["etat"],
+            data[key]["latitude"],
+            data[key]["longitude"]);
+        terrains.add(terrain);
+      }
+      setState(() {
+        print('lenght : ${terrains.length}');
+      });
+    });
+
+    super.initState();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     //Now we need multiple widgets into a parent = "Container" widget
     Widget titleSection = new Container(
-      padding: const EdgeInsets.all(10.0),//Top, Right, Bottom, Left
+      padding: const EdgeInsets.all(10.0), //Top, Right, Bottom, Left
       child: new Row(
         children: <Widget>[
           new Expanded(
@@ -27,16 +61,12 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.only(bottom: 10.0),
                   child: new Text("Trouve ton terrain",
                       style: new TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0
-                      )),
+                          fontWeight: FontWeight.bold, fontSize: 18.0)),
                 ),
                 //Need to add space below this Text ?
-                new Text("Tout les terrains ici",
-                  style: new TextStyle(
-                      color: Colors.grey[850],
-                      fontSize: 16.0
-                  ),
+                new Text(
+                  "Tout les terrains ici",
+                  style: new TextStyle(color: Colors.grey[850], fontSize: 16.0),
                 ),
               ],
             ),
@@ -51,65 +81,99 @@ class _HomePageState extends State<HomePage> {
           new Icon(icon, color: tintColor),
           new Container(
             margin: const EdgeInsets.only(top: 5.0),
-            child: new Text(buttonTitle, style: new TextStyle(fontSize: 16.0,
-                fontWeight: FontWeight.w600, color: tintColor),),
+            child: new Text(
+              buttonTitle,
+              style: new TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                  color: tintColor),
+            ),
           )
         ],
       );
     }
+
+    Widget TerrainList(BuildContext context) {
+      return Container(
+        height: MediaQuery.of(context).size.height * 0.20,
+        width: MediaQuery.of(context).size.width,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (context, index){
+            return Container(
+              margin: EdgeInsets.all(5),
+              height: MediaQuery.of(context).size.height * 0.10,
+              width: MediaQuery.of(context).size.width * 0.4,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+
+                boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(1.0, 3.0),
+                      blurRadius: 6.0,
+                    ),
+                  ],
+              ),
+            );
+          },
+        ),
+      );
+    }
+
     Widget ButtonsMapSection = new Container(
       child: new Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           //build in a separated function
           IconButton(
-            icon: Icon(Icons.map),
-            onPressed: () {
-              Navigator.push(context, new MaterialPageRoute(
-                  builder: (context) => Map()
-              ));
-            }
-          )
+              icon: Icon(Icons.map),
+              onPressed: () {
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (context) => Map()));
+              })
         ],
       ),
     );
+
     Widget fourButtonsSection = new Container(
       child: new Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           //build in a separated function
           IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () {
-              Navigator.push(context, new MaterialPageRoute(
-                  builder: (context) => HandballPage()
-              ));
-            }
-          ),
+              icon: Icon(Icons.home),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => HandballPage()));
+              }),
           IconButton(
               icon: Icon(Icons.home),
               onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => FootballPage()
-                ));
-              }
-          ),
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => FootballPage()));
+              }),
           IconButton(
               icon: Icon(Icons.home),
               onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => BasketballPage()
-                ));
-              }
-          ),
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => BasketballPage()));
+              }),
           IconButton(
               icon: Icon(Icons.home),
               onPressed: () {
-                Navigator.push(context, new MaterialPageRoute(
-                    builder: (context) => VolleyballPage()
-                ));
-              }
-          ),
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (context) => VolleyballPage()));
+              }),
         ],
       ),
     );
@@ -117,31 +181,28 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.all(20.0),
       //How to show long text ?
       child: new Text(''' Toutes les actualités ici ''',
-          style: new TextStyle(
-              color: Colors.grey[850],
-              fontSize: 16.0
-          )
-      ),
+          style: new TextStyle(color: Colors.grey[850], fontSize: 16.0)),
     );
     //build function returns a "Widget"
     return new MaterialApp(
-        title: "",
-        home: new Scaffold(
-            appBar: AppBar(
-              backgroundColor: Color.fromRGBO(0, 125, 0, 1),
-              toolbarHeight: 0,
-            ),
-            body: new ListView(
-              children: <Widget>[
-                //You can add more widget bellow
-                titleSection,
-                ButtonsMapSection,
-                fourButtonsSection,
-                bottomTextSection
-              ],
-            )
-        )
-    );//Widget with "Material design"
+      title: "",
+      home: new Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(0, 125, 0, 1),
+          toolbarHeight: 0,
+        ),
+        body: new ListView(
+          children: <Widget>[
+            //You can add more widget bellow
+            titleSection,
+            TerrainList(context),
+            ButtonsMapSection,
+            fourButtonsSection,
+            bottomTextSection
+          ],
+        ),
+      ),
+    ); //Widget with "Material design"
   }
 }
 
